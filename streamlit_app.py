e99905d4f9717e65c013dd82e330db06aec481bf
thecraigoneill <30849698+thecraigoneill@users.noreply.github.com>
2021-04-27 21:04:27 +1000

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import base64 
+import altair as alt
 
 st.title('Gravity Model')
 
@@ -63,6 +65,17 @@ def gz(xp, zp, x1,x2,z1,z2, dens):
         res = res * SI2MGAL * 2.0 * G
         return res
 
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'
+    return href
+
+
 xp = np.linspace(0,100,100)
 zp = np.zeros_like(xp)
 
@@ -82,14 +95,50 @@ density=float(density)
 line = gz(xp,zp,x1,x2,y1,y2,density)
 #line=zp
 
-st.write("Box extent, X:",x1,"-",x2," and Y:",y1,"-",y2)
-st.write("Box density:",density)
+st.write("Box extent, X:",x1,"-",x2," and Y:",y1,"-",y2, "Box density:",density)
 #if st.button("Calculate"):
 #    line = gz(xp,zp,x1,x2,y1,y2,density)
 #    line=zp
 
+x = np.array([x1,x1,x2,x2,x1])
+y = np.array([y1,y2,y2,y1,y1])
+
+#box = pd.DataFrame(data=np.column_stack((x,y)),columns=['X','Y'])
+#c=alt.Chart(box).mark_line().encode(x='X',y="Y",color=alt.Color('X', scale=alt.Scale(scheme='dark2'))
+#        scale=alt.Scale(domain=(0, 100))
+#        )
+box = pd.DataFrame({'x1': [x1], 'x2': [x2], 'y1': [y1], 'y2': [y2]})
+
+c = alt.Chart(box,width=600,height=400).mark_rect(fill='red', stroke='red').encode(
+    alt.X('x1', scale=alt.Scale(domain=(0, 100)),title="Distance (m)"),
+    alt.Y('y1', scale=alt.Scale(domain=(60, 0)),title="Depth (m)"),
+    x2='x2',
+    y2='y2').configure_axis(
+                labelFontSize=18,
+                titleFontSize=18
+)
+
+st.write("Box position")
+
+st.altair_chart(c)
+
 chart_data = pd.DataFrame(data=np.column_stack((xp,line)),columns=['Distance (m)','Gravity (mGal)'])
-st.line_chart(chart_data.rename(columns={'Distance (m)':'index'}).set_index('index'))
+#st.line_chart(chart_data.rename(columns={'Distance (m)':'index'}).set_index('index'),width=600)
+
+domain = ['Distance (m)']
+range_ = ['blue']
+line_chart = alt.Chart(chart_data,width=600,height=400).mark_line(interpolate='basis').encode(
+    alt.X('Distance (m)', title='Distance (m)'),
+    alt.Y('Gravity (mGal)', title='Gravity (mGal)'),
+    color=alt.value('blue')).properties(
+    title='Gravity of a box').configure_axis(
+                labelFontSize=18,
+                titleFontSize=18
+)
+
+# scale=alt.Scale(scheme='blueorange', domain=[50, 100])
+st.altair_chart(line_chart)
+
 
 #alt.Chart(source).mark_line().encode(
 #    x='x',
@@ -99,15 +148,19 @@ st.line_chart(chart_data.rename(columns={'Distance (m)':'index'}).set_index('ind
 #st.line_chart(line)
 
 if st.checkbox('Show dataframe'):
-    chart_data1 = pd.DataFrame(data=xp,columns=['Distance (m)'])
-    chart_data2 = pd.DataFrame(data=line,columns=['Gravity (mGal)'])
-    st.write("Distance (m)\n")
+    chart_data = pd.DataFrame(data=np.column_stack((xp,line)),columns=['Distance (m)','Gravity (mGal)'])
+    #chart_data1 = pd.DataFrame(data=xp,columns=['Distance (m)'])
+    #chart_data2 = pd.DataFrame(data=line,columns=['Gravity (mGal)'])
+    #st.write("Distance (m)\n")
     #st.write(np.c_[xp])
-    st.dataframe(chart_data1,510,2600)
-    st.write("Gravity (mGal)\n")
-    st.dataframe(chart_data2,610,2600)
+    #st.dataframe(chart_data1,510,2600)
+    #st.write("Gravity (mGal)\n")
+    #st.dataframe(chart_data2,610,2600)
+    st.dataframe(chart_data,610,2600)
 
     #st.write(chart_data2)
     #st.write(np.c_[line])
+
+st.markdown(get_table_download_link(chart_data), unsafe_allow_html=True)
     
 
