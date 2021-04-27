a0088769aa358121db341a41e8a358f11bdca994
thecraigoneill <30849698+thecraigoneill@users.noreply.github.com>
2021-04-27 22:04:01 +1000

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,7 @@ x1=st.sidebar.text_input('Input X1 here',30)
 x2=st.sidebar.text_input('Input X2 here',40)
 y1=st.sidebar.text_input('Input Y1 here',10)
 y2=st.sidebar.text_input('Input Y2 here',20)
-density=st.sidebar.text_input('Input Model Density contrast here',2000)
+density=st.sidebar.text_input('Input Model Density contrast here (background - body)',2000)
 
 
 x1=float(x1)
